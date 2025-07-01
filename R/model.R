72436c7090e4704315ffda2940ec3f1a70591801
amyheather <a.heather2@exeter.ac.uk>
2025-07-01 16:15:26 +0100

    feat(logging): add control of logging, to log_to_console and log_to_file control whether logs are shown and where, and the log message is appended with the parameters used

--- R/model.R
+++ R/model.R
@@ -6,8 +6,8 @@
 #' may not wish to do if being set elsewhere - such as done in \code{runner()}).
 #' Default is TRUE.
 #'
-#' @importFrom simmer get_attribute get_mon_arrivals get_mon_resources
-#' @importFrom simmer set_attribute simmer timeout wrap
+#' @importFrom simmer get_mon_arrivals get_mon_resources simmer wrap
+#' @importFrom utils capture.output
 #'
 #' @return TBC
 #' @export
@@ -19,12 +19,15 @@ model <- function(run_number, param, set_seed = TRUE) {
     set.seed(run_number)
   }
 
+  # Determine whether to get verbose activity logs
+  verbose <- any(c(param[["log_to_console"]], param[["log_to_file"]]))
+
   # Transform LOS parameters to lognormal scale
   param[["asu_los_lnorm"]] <- transform_to_lnorm(param[["asu_los"]])
   param[["rehab_los_lnorm"]] <- transform_to_lnorm(param[["rehab_los"]])
 
   # Create simmer environment
-  env <- simmer("simulation", verbose = TRUE)
+  env <- simmer("simulation", verbose = verbose)
 
   # Add ASU and rehab direct admission patient generators
   for (unit in c("asu", "rehab")) {
@@ -38,20 +41,39 @@ model <- function(run_number, param, set_seed = TRUE) {
       }
 
       # Add patient generator using the created trajectory
-      env <- add_patient_generator(
-        env = env,
-        trajectory = traj,
-        unit = unit,
-        patient_type = patient_type,
-        param = param
+      sim_log <- capture.output(
+        env <- add_patient_generator(
+          env = env,
+          trajectory = traj,
+          unit = unit,
+          patient_type = patient_type,
+          param = param
+        )
       )
     }
   }
 
   # Run the model
-  env <- env |>
-    simmer::run(20L) |>
-    wrap()
+  sim_log <- capture.output(
+    env <- env |>
+      simmer::run(20L) |>
+      wrap()
+  )
+
+  # Save and/or display the log
+  if (isTRUE(verbose)) {
+    # Create full log message by adding parameters
+    param_string <- paste(names(param), param, sep = "=", collapse = ";\n ")
+    full_log <- append(c("Parameters:", param_string, "Log:"), sim_log)
+    # Print to console
+    if (isTRUE(param[["log_to_console"]])) {
+      print(full_log)
+    }
+    # Save to file
+    if (isTRUE(param[["log_to_file"]])) {
+      writeLines(full_log, param[["file_path"]])
+    }
+  }
 
   # Extract the monitored arrivals and resources information from the simmer
   # environment object
